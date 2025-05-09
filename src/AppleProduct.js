0c5c8501c2490696df96f10d478cc09b0871347b
Dr. Alberto Aguilar González <alberto.aguilar@tec.mx>
2025-05-09 12:49:22 -0600

    Update AppleProduct.js

--- src/AppleProduct.js
+++ src/AppleProduct.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 
 import iphone from './svg/iphone.svg';
 import ipad from './svg/ipad.svg';
@@ -6,16 +7,23 @@ import watch from './svg/watch.svg';
 import imac from './svg/imac.svg';
 import macbook from './svg/macbook.svg';
 import airpods from './svg/airpods.svg';
+import heart from './svg/heart.svg'
 
 const mapaSVG = {
   iphone, ipad, watch, imac, macbook, airpods
 };
 
 export function AppleProduct( props ) {
+
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
-    <div>
-      Producto {props.type}
+    <div onClick={handleClick}>
       <img alt="producto" src={mapaSVG[props.type]} width={200} />
+      <img alt="heart" src={heart} style={{ width:10 + 10*clicks + 'px'}}/>
     </div>
   )
-}
+};
